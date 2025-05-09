b47fc7f397b228fb236a5ba5eacb206300bc1970
A7medBibars <ahmed.bebars2001@gmail.com>
2025-05-09 16:02:53 +0300

    fix path

--- src/modules/media/ai.service.js
+++ src/modules/media/ai.service.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { promisify } from 'util';
 import AppError from '../../utils/appError.js';
-import { messages } from '../../utils/constants/response.js';
+import { messages } from '../../utils/constants/messages.js';
 import { aiConfig } from '../../config/ai.config.js';
 import FormData from 'form-data';
 
